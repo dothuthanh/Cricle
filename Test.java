1bbf5d2cc5bc4775ef1c21fb8061ae46b0667cdd
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-03-26 21:27:43 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+package codegym.com;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle a = new Circle();
+        System.out.println(a);
+        Circle a1 = new Circle(3.0, "black");
+        System.out.println(a1);
+    }
+}
